95889869f622931e445870d1d4a74a0cae7e5840
lyn2helloworld <34390567+lyn2helloworld@users.noreply.github.com>
2018-03-02 21:04:50 +0800

    Create EmployeeAction.java

---
+++ com/lyn/oa/web/action/EmployeeAction.java
@@ -0,0 +1,14 @@
+package com.lyn.oa.web.action;
+
+
+
+//处理Employee相关的请求
+public class EmployeeAction {
+	
+	public String execute(){
+		System.out.println("EmployeeAction.list()");
+		return "list";
+		
+	}
+
+}
