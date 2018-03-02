ac50af93d3abd1baa64670ce39b460422005869f
lyn2helloworld <34390567+lyn2helloworld@users.noreply.github.com>
2018-03-02 20:55:35 +0800

    Create DepartmentAction.java

---
+++ com/lyn/oa/web/action/DepartmentAction.java
@@ -0,0 +1,11 @@
+package com.lyn.oa.web.action;
+//处理Department相关的请求
+public class DepartmentAction {
+	
+	public String execute(){
+		
+		System.out.println("DepartmentAction.list()");
+		return "list";
+	}
+
+}
